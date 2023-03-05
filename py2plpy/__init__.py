1c4a11e9f4467c090d268672d9f10cfb777eaa7d
Marc Endesfelder <marc.endesfelder@gmail.com>
2023-03-05 15:49:55 +0100

    Add support for argument modifiers

--- py2plpy/__init__.py
+++ py2plpy/__init__.py
@@ -1,2 +1,2 @@
-from .py2plpy import transform
+from .py2plpy import transform, Out, In, InOut, SetOf, Record, sql_properties
 from .plpy import plpy
--- py2plpy/py2plpy.py
+++ py2plpy/py2plpy.py
@@ -4,12 +4,38 @@ import sys
 import importlib
 import re
 from lark import Lark
+from typing import Generic, TypeVar, List
+try:
+    from typing import Literal
+except ImportError:
+    from typing_extensions import Literal
+
+
 
 l = Lark.open(os.path.dirname(os.path.realpath(__file__))+'/grammar.lark')
 
+
+class Modifier:
+    def __init__(self, type):
+        self.type = type
+
+T = TypeVar('T')
+class Out(Generic[T], Modifier):
+    pass
+class In(Generic[T], Modifier):
+    pass
+class InOut(Generic[T], Modifier):
+    pass
+class SetOf(Generic[T], Modifier):
+    pass
+
+class Record:
+    pass
+
+
 p = '''CREATE OR REPLACE FUNCTION {schema}.{name} (
 {arguments})
-RETURNS {returntype}
+{returntype}
 LANGUAGE 'plpython3u' 
 {properties}
 AS $BODY$
@@ -17,25 +43,57 @@ AS $BODY$
 $BODY$;'''
 
 types = {
-    int : 'INTEGER',
-    str : 'TEXT',
-    float : 'DOUBLE',
-    bool : 'BOOLEAN'
+    int: 'INTEGER',
+    str: 'TEXT',
+    float: 'DOUBLE',
+    bool: 'BOOLEAN',
+    Record: 'RECORD'
 }
 
-def convertType(t):
-    try:
+
+def convertType_(t):
+    if t in types:
         return types[t]
-    except KeyError:
-        pass
-    try:
-        if t.__origin__ == list:
-            return types[t.__args__[0]]+'[]'
-    except:
-        return t.__name__
+    if hasattr(t, '__origin__') and t.__origin__ == list:
+        return convertType_(t.__args__[0])+'[]'
+    return t.__name__
+
+
+def convertType(t):
+    if hasattr(t, '__origin__'):
+        if t.__origin__ == Out:
+            return convertType_(t.__args__[0]), 'OUT '
+        elif t.__origin__ == In:
+            return convertType_(t.__args__[0]), 'IN '
+        elif t.__origin__ == InOut:
+            return convertType_(t.__args__[0]), 'INOUT '
+        elif t.__origin__ == SetOf:
+            return convertType_(t.__args__[0]), 'SETOF '
+    return convertType_(t), ''
+
+def param(name, t):
+    type, prefix = convertType(t)
+    return prefix + name + ' ' + type
+
+def returntype(t):
+    type, prefix = convertType(t)
+    return prefix + type
+
 
 def getBody(f):
-    body = re.sub(r'^([^\n\(]|\([^\)]*\))+\n', '', inspect.getsource(f)).rstrip()
+    body = inspect.getsource(f)
+    level = 0
+    i = 0
+    for c in body:
+        if c == ':' and level == 0:
+            break
+        elif c == '(':
+            level += 1
+        elif c == ')':
+            level -= 1
+        i += 1
+    body = body[i+1:].rstrip()
+
     s = re.match(r'^ *[ruRU]?(\'\'\'|"""|\'|")', body)
     if s:
         i = body[s.end():].find(s.group(1)) + s.end() + len(s.group(1))
@@ -43,51 +101,52 @@ def getBody(f):
     return body
 
 
-def parseDoc(f):
-        tree = l.parse(inspect.getdoc(f).upper())
-        properties = {}
-        for s in tree.iter_subtrees():
-            if s.data == 'strict':
-                properties['strictness'] = 'STRICT'
-            elif s.data == 'nonstrict':
-                properties['strictness'] = 'CALLED ON NULL INPUT'
-            elif s.data == 'volatile':
-                properties['volatility'] = 'VOLATILE'
-            elif s.data == 'immutable':
-                properties['volatility'] = 'IMMUTABLE'
-            elif s.data == 'stable':
-                properties['volatility'] = 'STABLE'
-            elif s.data == 'parasafe':
-                properties['parallelism'] = 'PARALLEL SAFE'
-            elif s.data == 'paraunsafe':
-                properties['parallelism'] = 'PARALLEL UNSAFE'
-            elif s.data == 'pararestricted':
-                properties['parallelism'] = 'PARALLEL RESTRICTED'
-            elif s.data == 'invoker':
-                properties['security'] = 'EXTERNAL SECURITY INVOKER'
-            elif s.data == 'definer':
-                properties['security'] = 'EXTERNAL SECURITY DEFINER'
-            elif s.data == 'leakproof':
-                properties['leakiness'] == 'LEAKPROOF'
-            elif s.data == 'nonleakproof':
-                properties['leakiness'] == 'NOT LEAKPROOF'
-            elif s.data == 'cost':
-                properties['cost'] = 'COST '+s.children[0]
-            elif s.data == 'rows':
-                properties['rows'] = 'ROWS '+s.children[0]
-            elif s.data == 'transform':
-                properties['transform'] = 'TRANSFORM' + (' FOR TYPE '+s.children[0] if len(s.children) else '')
-        return '\n'.join(properties.values())
+def sql_properties(
+        strict:bool = False, 
+        volatility:Literal['volatile', 'stable', 'immutable'] = None, 
+        parallel:Literal['safe', 'unsafe', 'restricted'] = None, 
+        leakproof:bool = None, 
+        cost:int = None,
+        rows:int = None,
+        transform:List[type]=[]):
+    properties = []
+    if strict == True:
+        properties.append('STRICT')
+    elif strict == False:
+        properties.append('CALLED ON NULL INPUT')
+    if volatility:
+        properties.append(volatility.upper())
+    if parallel:
+        properties.append(parallel.upper())
+    if leakproof == True:
+        properties.append('LEAKPROOF')
+    elif leakproof == False:
+        properties.append('NOT LEAKPROOF')
+    if cost is not None:
+        properties.append(f'COST {cost}')
+    if rows is not None:
+        properties.append(f'ROWS {rows}')
+    if transform:
+        properties.append('TRANSFORM FOR TYPE '+', FOR TYPE '.join([convertType_(t) for t in transform]))
+
+    def f(func):
+        func._sqlProperties = '\n'.join(properties)
+        return func
+    
+    return f
 
 
 def transformFunc(f, schema = 'public'):
     d = {}
     s = inspect.signature(f)
-    d['arguments'] = ',\n'.join([param.name+' '+convertType(param.annotation) for param in s.parameters.values()])
-    d['returntype'] = convertType(s.return_annotation)
+    d['arguments'] = ',\n'.join([param(p.name, p.annotation) for p in s.parameters.values()])
+    d['returntype'] = 'RETURNS ' + returntype(s.return_annotation) if s.return_annotation != inspect.Signature.empty else ''
     d['body'] = getBody(f)
     d['name'] = f.__name__
-    d['properties'] = parseDoc(f)
+    try:
+        d['properties'] = f._sqlProperties
+    except AttributeError:
+        d['properties'] = ''
     d['schema'] = schema
     return p.format_map(d) 
     
@@ -99,7 +158,8 @@ def transform(path):
     m = importlib.import_module(name)
 
     res = ''
-    for _, obj in inspect.getmembers(m, inspect.isroutine):
-        res += transformFunc(obj)
+    for _, f in inspect.getmembers(m, inspect.isroutine):
+        if f.__module__ == m.__name__:
+            res += transformFunc(f)
 
     return res.replace('\n', '\n    ')
